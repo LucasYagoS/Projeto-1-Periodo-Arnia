dbc423473d1726751f610f8bf5a0f012c080f977
Lucas Yago <lucasyago2010@gmail.com>
2023-04-16 18:45:15 -0300

    dia 14

--- app.js
+++
@@ -1,4 +0,0 @@
-function next() {
-    const rola = document.getElementById("div1").style.display = "none"
-    const caralho = document.getElementById("div2").style.display = "block"
-}
--- exemplo/app.js
+++
@@ -1,9 +0,0 @@
-function next() {
-    const rola = document.getElementById("rola")
-    const caralho = document.getElementById("caralho")
-    
-    rola.innerHTML.className = "caralho"
-    caralho.innerHTML.className = "rola"
-    console.log(rola)
-    console.log(caralho)
-}
